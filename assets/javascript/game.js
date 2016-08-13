df00ee9b189eab2c70944e1fdacc8f17184eda03
Allie James <alliedaniellejames@gmail.com>
2016-08-13 13:54:02 -0500

    workinprogress

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Psychic Game</title>
+</head>
+<body>
+<script type="text/javascript">
+<div id="game">
+
+</div>
+
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z"]
+
+var Wins = 0:
+var Losses = 0;
+var Guesses Left =0;
+
+
+	var computerGuess =Math.floor(Math.random("options.Length)];
+	var userGuess = prompt("Guess what letter I am thinking of");
+
+function myFunction
+
+document.onkeyup = function(event) {
+	alert('pressed a button');
+	var userGuess = 
+
+document.write ("Guess what letter I am thinking of");
+	if ((userGuess == computerGuess)){
+		Wins++;
+	}else if ((userGuess != computerGuess)){
+		Losses++;
+	}
+
+}
+
+</script>
+</body>
+</html>
