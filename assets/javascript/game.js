b8d28beb6772de45d0293eabfd8fbd942f4edc01
Allie James <alliedaniellejames@gmail.com>
2016-08-13 14:51:12 -0500

    workinprogress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Psychic Game</title>
-</head>
-<body>
-<script type="text/javascript">
-<div id="game">
-
-</div>
-
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z"]
 
-var Wins = 0:
+var Wins = 0;
 var Losses = 0;
-var Guesses Left =0;
+var GuessesLeft =9;
 
 
-	var computerGuess =Math.floor(Math.random("options.Length)];
-	var userGuess = prompt("Guess what letter I am thinking of");
-
-function myFunction
-
-document.onkeyup = function(event) {
-	alert('pressed a button');
-	var userGuess = 
-
-document.write ("Guess what letter I am thinking of");
+var computerGuess = options[Math.floor(Math.random(options.Length))];
+console.log (options[computerGuess]); 
+console.log (computerGuess);
+var userGuess = prompt("Guess what letter I am thinking of");
 	if ((userGuess == computerGuess)){
 		Wins++;
 	}else if ((userGuess != computerGuess)){
-		Losses++;
+		GuessesLeft--;
 	}
-
+	console.log(userGuess);
+	console.log(Wins);
+	console.log(GuessesLeft);
+document.onkeyup = function(event) {
+	alert('pressed a button');
+	var userGuess = 9;
 }
-
-</script>
-</body>
-</html>
